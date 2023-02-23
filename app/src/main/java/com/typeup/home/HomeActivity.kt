7267111eab1e0f7ec4958598e6817736657f4e7b
Marko Mihajlovic <mmaker.biz@gmail.com>
2023-02-23 17:08:36 +0100

    empty HomeActivity

---
+++ app/src/main/java/com/typeup/home/HomeActivity.kt
@@ -0,0 +1,15 @@
+package com.typeup.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.typeup.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+    }
+
+}
