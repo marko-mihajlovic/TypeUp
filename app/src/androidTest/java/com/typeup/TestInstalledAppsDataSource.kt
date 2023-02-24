72ad88906e7333613e8c1bc6cbb5eb3142c0be36
Marko Mihajlovic <mmaker.biz@gmail.com>
2023-02-24 19:35:12 +0100

    renamed

--- app/src/androidTest/java/com/typeup/TestInstalledAppsDataSource.kt
+++ app/src/androidTest/java/com/typeup/TestInstalledAppsDataSource.kt
@@ -2,19 +2,19 @@ package com.typeup
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import com.typeup.home.AppsDataSource
+import com.typeup.home.InstalledAppsDataSource
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.*
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class TestAppsDataSource {
+class TestInstalledAppsDataSource {
 
     @Test
     fun test_there_are_apps() {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        val list = AppsDataSource.get(appContext)
+        val list = InstalledAppsDataSource.get(appContext)
         assertThat(list, `is`(not(empty())))
     }
 
@@ -22,7 +22,7 @@ class TestAppsDataSource {
     fun test_some_app_exists() {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
 
-        val list = AppsDataSource.get(appContext)
+        val list = InstalledAppsDataSource.get(appContext)
         assertThat(
             list.firstOrNull { x ->
                 x.appName == "Gmail"
