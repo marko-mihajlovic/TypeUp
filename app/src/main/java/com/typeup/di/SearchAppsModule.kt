5d6384f32fb9bd3bd2666d70104641668a5d9c95
Marko Mihajlovic <mmaker.biz@gmail.com>
2023-03-03 16:03:51 +0100

    di

--- app/src/main/java/com/typeup/di/SearchAppsModule.kt
+++ app/src/main/java/com/typeup/di/SearchAppsModule.kt
@@ -1,26 +1,33 @@
 package com.typeup.di
 
+import android.content.Context
 import com.typeup.search_apps.data.data_source.InstalledAppsDataSource
 import com.typeup.search_apps.data.data_source.InstalledAppsDataSourceImpl
 import com.typeup.search_apps.data.repo.InstalledAppsRepo
 import com.typeup.search_apps.data.repo.InstalledAppsRepoImpl
-import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-interface SearchAppsModule {
+object SearchAppsModule {
 
-    @Binds
-    fun bindInstalledAppsRepo(
-        installedAppsRepoImpl: InstalledAppsRepoImpl
-    ): InstalledAppsRepo
+    @Provides
+    fun provideRepo(
+        @ApplicationContext context: Context,
+        dataSource: InstalledAppsDataSource
+    ): InstalledAppsRepo {
+        return InstalledAppsRepoImpl(context, dataSource)
+    }
 
-    @Binds
-    fun bindInstalledAppsDataSource(
-        installedAppsDataSourceImpl: InstalledAppsDataSourceImpl
-    ): InstalledAppsDataSource
+    @Provides
+    fun provideDataSource(
+        @ApplicationContext context: Context
+    ): InstalledAppsDataSource {
+        return InstalledAppsDataSourceImpl(context)
+    }
 
 }
