ab1c66966c6111497115724a57fc1a609326bb52
Marko Mihajlovic <mmaker.biz@gmail.com>
2023-03-04 20:03:07 +0100

    add different behavior if item is our app

--- app/src/androidTest/java/com/typeup/search_apps/TestSearchActivity.kt
+++ app/src/androidTest/java/com/typeup/search_apps/TestSearchActivity.kt
@@ -1,10 +1,14 @@
 package com.typeup.search_apps
 
+import android.content.Intent
+import android.provider.Settings
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.PositionAssertions.isCompletelyBelow
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -55,8 +59,50 @@ class TestSearchActivity {
         }
 
         ActivityScenario.launch(SearchActivity::class.java).use {
+            Intents.init()
+
             onView(searchInput).perform(typeText("o"))
             onView(listView).check(matches(withListSize(MaxShownItems.default)))
+
+            onData(anything()).atPosition(0).perform(click())
+            val intents = Intents.getIntents()
+            assertThat(intents.size, `is`(1))
+            assertThat(intents[0].action.equals(Intent.ACTION_MAIN), `is`(true))
+            assertThat(intents[0].hasCategory(Intent.CATEGORY_LAUNCHER), `is`(true))
+
+            Intents.release()
+        }
+
+        ActivityScenario.launch(SearchActivity::class.java).use {
+            Intents.init()
+
+            onView(searchInput).perform(typeText("typeup"))
+            onData(anything()).atPosition(0).perform(click())
+
+            val appInfoBtn = onView(
+                allOf(
+                    withId(android.R.id.text1), withText(context.getString(R.string.appInfoTxt)),
+                    withParent(
+                        allOf(
+                            withId(androidx.appcompat.R.id.select_dialog_listview),
+                            withParent(withId(androidx.appcompat.R.id.contentPanel))
+                        )
+                    ),
+                    isDisplayed()
+                )
+            )
+            appInfoBtn
+                .check(matches(withText(context.getString(R.string.appInfoTxt))))
+                .perform(click())
+
+            val intents = Intents.getIntents()
+            assertThat(intents.size, `is`(1))
+            assertThat(
+                intents[0].action.equals(Settings.ACTION_APPLICATION_DETAILS_SETTINGS),
+                `is`(true)
+            )
+
+            Intents.release()
         }
 
     }
