bc67a1f3a877a41b14d9ccd92abf08febb13c575
Marko Mihajlovic <mmaker.biz@gmail.com>
2023-03-04 21:45:58 +0100

    extracted onView() calls

--- app/src/androidTest/java/com/typeup/search_apps/TestSearchActivity.kt
+++ app/src/androidTest/java/com/typeup/search_apps/TestSearchActivity.kt
@@ -27,42 +27,44 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class TestSearchActivity {
 
+    private fun searchInput() = withId(R.id.searchInput)
+
+    private fun onAcceptBtn() = onView(withText("Accept"))
+    private fun onSearchInput() = onView(searchInput())
+    private fun onListView() = onView(withId(R.id.listView))
+    private fun onMsgTxt() = onView(withId(R.id.msgTxt))
+
     @Test
     fun test_activity() {
-
-        val searchInput = withId(R.id.searchInput)
-        val listView = withId(R.id.listView)
-        val msgTxt = withId(R.id.msgTxt)
 
         val context = InstrumentationRegistry.getInstrumentation().targetContext
         SharedPref.edit(context).clear().commit()
 
         ActivityScenario.launch(SearchActivity::class.java).use {
 
-            val acceptBtn = withText("Accept")
-            onView(acceptBtn).perform(click())
+            onAcceptBtn() .perform(click())
 
-            onView(listView).check(isCompletelyBelow(searchInput))
+            onListView().check(isCompletelyBelow(searchInput()))
 
-            onView(searchInput).perform(typeText("gm")) // gmail
-            onView(listView).check(matches(withListSize(1)))
+            onSearchInput().perform(typeText("gm")) // gmail
+            onListView().check(matches(withListSize(1)))
 
-            onView(searchInput).perform(replaceText("non_existent_app"))
-            onView(listView).check(matches(withListSize(0)))
-            onView(msgTxt).check(matches(withText("No results found.")))
-            onView(msgTxt).check(isCompletelyBelow(searchInput))
+            onSearchInput().perform(replaceText("non_existent_app"))
+            onListView().check(matches(withListSize(0)))
+            onMsgTxt().check(matches(withText("No results found.")))
+            onMsgTxt().check(isCompletelyBelow(searchInput()))
 
-            onView(searchInput).perform(clearText())
+            onSearchInput().perform(clearText())
 
-            onView(searchInput).perform(typeText("a"))
-            onView(listView).check(matches(withListSize(MaxShownItems.default)))
+            onSearchInput().perform(typeText("a"))
+            onListView().check(matches(withListSize(MaxShownItems.default)))
         }
 
         ActivityScenario.launch(SearchActivity::class.java).use {
             Intents.init()
 
-            onView(searchInput).perform(typeText("o"))
-            onView(listView).check(matches(withListSize(MaxShownItems.default)))
+            onSearchInput().perform(typeText("o"))
+            onListView().check(matches(withListSize(MaxShownItems.default)))
 
             onData(anything()).atPosition(0).perform(click())
             val intents = Intents.getIntents()
@@ -76,8 +78,8 @@ class TestSearchActivity {
         ActivityScenario.launch(SearchActivity::class.java).use {
             Intents.init()
 
-            onView(searchInput).perform(typeText("o"))
-            onView(listView).check(matches(withListSize(MaxShownItems.default)))
+            onSearchInput().perform(typeText("o"))
+            onListView().check(matches(withListSize(MaxShownItems.default)))
 
             onData(anything()).atPosition(0).perform(longClick())
             onAppInfoBtn(context).perform(click())
@@ -94,7 +96,7 @@ class TestSearchActivity {
         ActivityScenario.launch(SearchActivity::class.java).use {
             Intents.init()
 
-            onView(searchInput).perform(typeText("typeup"))
+            onSearchInput().perform(typeText("typeup"))
             onData(anything()).atPosition(0).perform(click())
 
             onAppInfoBtn(context).perform(click())
